8dd6ffe5275712c2f5fce2cda86a263d30fd15fa
jkdihenkar <jkdihenkar@gmail.com>
2024-06-21 11:40:08 +0530

    add reflection on gRPC server

--- grpc-calculator/src/main.rs
+++ grpc-calculator/src/main.rs
@@ -1,8 +1,13 @@
 use proto::calculator_server::{Calculator, CalculatorServer};
 use tonic::transport::Server;
+use tonic_reflection;
 
 mod proto {
     tonic::include_proto!("calculator");
+
+    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
+        tonic::include_file_descriptor_set!("calculator_descriptor");
+
 }
 
 #[derive(Debug, Default)]
@@ -31,7 +36,12 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let addr = "[::1]:50051".parse()?;
     let calc = CalculatorService::default();
     
+    let service = tonic_reflection::server::Builder::configure()
+        .register_encoded_file_descriptor_set(proto::FILE_DESCRIPTOR_SET)
+        .build()?;
+
     Server::builder()
+        .add_service(service)
         .add_service(CalculatorServer::new(calc))
         .serve(addr)
         .await?;
