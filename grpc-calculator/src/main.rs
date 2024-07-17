0016cffd6ed7a336d2e5809a8d4bea46008bc9ef
jkdihenkar <jkdihenkar@gmail.com>
2024-07-17 07:22:09 +0530

    adding divide and handle the invalid args error

--- grpc-calculator/src/main.rs
+++ grpc-calculator/src/main.rs
@@ -28,6 +28,26 @@ impl Calculator for CalculatorService {
 
         Ok(tonic::Response::new(response))
     }
+
+    async fn divide(
+        &self,
+        request: tonic::Request<proto::CalculationRequest>,
+    ) -> Result<tonic::Response<proto::CalculationResponse>, tonic::Status> {
+        println!("Got a request: {:?}", request);
+
+        let input = request.get_ref();
+
+        if input.b == 0 {
+            return Err(tonic::Status::invalid_argument("Cannot Divide by 0!"))
+        }
+
+        let response = proto::CalculationResponse {
+            result: input.a / input.b,
+        };
+
+        Ok(tonic::Response::new(response))
+    }
+
 }
 
 #[tokio::main]
